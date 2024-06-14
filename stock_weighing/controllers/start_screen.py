93f4e6047e3f6c8798d4ef7cbe39973cac6e4633
javierjcf <javier@comunitea.com>
2024-06-14 16:05:27 +0200

    [MIG]stock_weighting: Fix controller rendering for v16

--- stock_weighing/controllers/start_screen.py
+++ stock_weighing/controllers/start_screen.py
@@ -3,15 +3,13 @@
 from odoo import http
 from odoo.http import request
 
-
 class StockWeighing(http.Controller):
     @http.route("/stock_weighing/start_screen", auth="user", type="json")
     def stock_weighing_start_screen(self):
         """Returns the banner for the weighing options"""
         return {
-            "html": request.env.ref(
-                "stock_weighing.stock_weighing_start_screen"
-            )._render(
+            "html": request.env['ir.qweb']._render(
+                "stock_weighing.stock_weighing_start_screen",
                 {
                     "actions": request.env[
                         "weigh.operation.selection"
