3b07db9d662528bc01161f38fe1ad31cd41ef27e
javierjcf <javier@comunitea.com>
2024-07-23 01:29:17 +0200

    [ADD]Button widget measure and reading from socket

--- web_widget_remote_measure/static/src/remote_measure/remote_measure.esm.js
+++ web_widget_remote_measure/static/src/remote_measure/remote_measure.esm.js
@@ -4,101 +4,6 @@ import {registry} from "@web/core/registry";
 import {standardFieldProps} from "@web/views/fields/standard_field_props";
 const {onMounted, onWillUnmount, useState, } = owl;
 
-
-// export class RemoteMeasureOwl extends FloatField {
-//     setup() {
-//         debugger;
-//         super.setup();
-//         this.state = useState({
-//             amount: 0,
-//         });
-
-//         // Extraer las propiedades adicionales
-//         this.remoteDeviceField = this.props.remote_device_field;
-//         this.measureDeviceId = this.props.measure_device_id;
-//         this.uomField = this.props.uom_field;
-
-//         onMounted(() => {
-//             this.connectToWebSocket();
-//         });
-
-//         onWillUnmount(() => {
-//             this.closeSocket();
-//         });
-//     }
-
-//     connectToWebSocket() {
-//         debugger;
-//         // const host = this.props.record.data[this.remoteDeviceField]?.host;
-//         const host = "ws://localhost:8765";
-//         try {
-//             this.socket = new WebSocket(host);
-//         } catch (error) {
-//             if (error.code === 18) {
-//                 return;
-//             }
-//             throw error;
-//         }
-//         this.socket.onmessage = async (msg) => {
-//             debugger;
-//             const data = await msg.data.text();
-//             const processedData = this.processMsg(data);
-//             this.state.amount = processedData.value;
-
-//             // Actualizar el valor del campo float
-//             this.props.update(this.state.amount);
-//         };
-
-//         this.socket.onerror = () => {
-//             console.error('WebSocket error');
-//         };
-//     }
-
-//     processMsg(msg) {
-//         debugger;
-//         // Implementar el procesamiento del mensaje según el protocolo
-//         // Aquí se asume un simple parseo del valor como ejemplo
-//         return {
-//             value: parseFloat(msg),
-//         };
-//     }
-
-//     closeSocket() {
-//         debugger;
-//         if (this.socket) {
-//             this.socket.close();
-//         }
-//     }
-// }
-
-// RemoteMeasureOwl.template = "owl_measure_device_status";
-
-// // Define solo las nuevas propiedades necesarias
-// RemoteMeasureOwl.props = {
-//     ...standardFieldProps,
-//     remote_device_field: { type: String, optional: true },
-//     measure_device_id: { type: String, optional: true },
-//     uom_field: { type: String, optional: true },
-// };
-
-// RemoteMeasureOwl.extractProps = ({ attrs }) => {
-//     return {
-//         remote_device_field: attrs.options.remote_device_field,
-//         measure_device_id: attrs.options.measure_device_id,
-//         uom_field: attrs.options.uom_field,
-//     };
-// };
-
-// registry.category("fields").add("remote_measure", RemoteMeasureOwl);
-
-
-
-
-
-
-
-
-
 const nextState = {
     "fa-thermometer-empty": "fa-thermometer-quarter",
     "fa-thermometer-quarter": "fa-thermometer-half",
@@ -116,8 +21,9 @@ export class RemoteMeasureOwl extends FloatField {
             amount: 0,
             icon: "fa-thermometer-empty",
             isStable: false,
+            isMeasurig: false,
         });
-        this.streamSuccessCounter = 5;
+        this.streamSuccessCounter = 10;
 
         // Extraer las propiedades adicionales
         this.remoteDeviceField = this.props.remote_device_field;
@@ -125,20 +31,21 @@ export class RemoteMeasureOwl extends FloatField {
         this.uomField = this.props.uom_field;
 
         onMounted(() => {
-            this.connectToWebSocket();
+            this._connect_to_websockets();
         });
 
         onWillUnmount(() => {
-            this.closeSocket();
+            this._closeSocket();
         });
     }
 
-    connectToWebSocket() {
-        debugger;
+    _connect_to_websockets() {
+        // debugger;
         const host = "ws://localhost:8765";
         try {
             this.socket = new WebSocket(host);
         } catch (error) {
+            // Avoid websockets security error. Local devices won't have wss normally
             if (error.code === 18) {
                 return;
             }
@@ -146,9 +53,9 @@ export class RemoteMeasureOwl extends FloatField {
         }
 
         this.socket.onmessage = async (msg) => {
-            debugger;
+            // debugger;
             const data = await msg.data.text();
-            const processedData = this.processMsg(data);
+            const processedData = this._proccess_msg_f501(data);
 
             // Manejar la estabilidad de la medida
             if (!processedData.stable) {
@@ -157,8 +64,8 @@ export class RemoteMeasureOwl extends FloatField {
 
             if (processedData.stable && this.streamSuccessCounter <= 0) {
                 this.state.isStable = true;
-                this.updateField(processedData.value);
-                this.closeSocket();
+                this._setMeasure(processedData.value);
+                this._closeSocket();
                 return;
             } else {
                 this.state.isStable = false;
@@ -168,7 +75,7 @@ export class RemoteMeasureOwl extends FloatField {
                 this.streamSuccessCounter--;
             }
 
-            this.state.icon = this.getNextStateIcon(this.state.icon);
+            this.state.icon = this._nextStateIcon(this.state.icon);
         };
 
         this.socket.onerror = () => {
@@ -176,7 +83,7 @@ export class RemoteMeasureOwl extends FloatField {
         };
     }
 
-    processMsg(msg) {
+    _proccess_msg_f501(msg) {
         debugger;
         // Implementar el procesamiento del mensaje según el protocolo
         // Aquí se asume un simple parseo del valor como ejemplo
@@ -186,22 +93,28 @@ export class RemoteMeasureOwl extends FloatField {
         };
     }
 
-    getNextStateIcon(currentIcon) {
+    _nextStateIcon(currentIcon) {
         return nextState[currentIcon];
     }
 
-    updateField(value) {
+    _setMeasure(value) {
         this.state.amount = value;
         this.props.update(this.state.amount);
     }
 
-    closeSocket() {
+    _closeSocket() {
         if (this.socket) {
             this.socket.close();
         }
     }
-    startMeasurement(){
-        this.connectToWebSocket();
+    _onMeasure() {
+        this.state.isMeasuring = true;
+        this._connect_to_websockets();
+    }
+
+    _onValidateMeasure() {
+        this.state.isMeasuring = false;
+        this._closeSocket();
     }
 }
 
