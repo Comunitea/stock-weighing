4ed01643064601c30f8374e305ebba001dc225f5
javierjcf <javier@comunitea.com>
2024-07-26 01:59:19 +0200

    [MIG]Manual tare

--- stock_weighing_remote_measure/static/src/remote_measure_form/remote_measure_form.esm.js
+++ stock_weighing_remote_measure/static/src/remote_measure_form/remote_measure_form.esm.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
 import {registry} from "@web/core/registry";
 import { FloatField } from "@web/views/fields/float/float_field";
+const {useState } = owl;
+
 
 // import {RemoteMeasureOwl} from "@web_widget_remote_measure/remote_measure/remote_measure";
 const RemoteMeasureOwl = registry.category("fields").get("remote_measure");
@@ -11,15 +13,103 @@ class RemoteMeasureFormOwl extends RemoteMeasureOwl {
         super.setup();
         this.tares = this.props.tares;
         this.tare = 0;
-        this.widget_amout = 69;
+        // this.amount = 0;
         this.number_format_options = {
             minimumFractionDigits: 3,
             useGrouping: false,
         };
+        this.state = useState({
+            ...this.state,
+            tare: 0,
+            real_amount: 0,
+            manual_tare: 0
+        });
     }
-    get remoteMeasureProps() {
-        const {tares, inputType, ...result} = this.props;
-        return result;
+    // get remoteMeasureProps() {
+    //     const {tares, inputType, ...result} = this.props;
+    //     return result;
+    // }
+
+    /**
+     * Number formatting helper
+     * @param {Number} weight
+     * @returns {String}
+     */
+    format_weight(weight) {
+        return weight.toLocaleString(this.locale_code, this.number_format_options);
+    }
+
+    /**
+     * Add the tare to the real weighted measure
+     * @override
+     */
+    //  async _setMeasure() {
+    //     debugger;
+    //     await this._super(...arguments);
+    //     // let total = this.widget_amount;
+    //     // this.$input.val(this.format_weight(total));
+    //     // if (this.tare) {
+    //     //     total = this.widget_amount - this.tare;
+    //     //     this.$real_amount.text(this.widget_amount);
+    //     //     this.$input.val(this.format_weight(total));
+    //     //     this._setValue(this.$input.val());
+    //     // }
+    // }
+
+    // updateField(val) {
+    //     return Promise.resolve(this.props.update(val));
+    // }
+
+    /**
+     * Add the tare to the real weighted measure
+     * @override
+     */
+    async _setMeasure(amount) {
+        if (isNaN(amount)) {
+            return;
+        }
+        this.state.real_amount = this.format_weight(amount);
+        let total = amount;
+        if (this.tare) {
+            total = amount - this.tare;
+            // this.$real_amount.text(this.amount);
+            // this.$input.val(this.format_weight(total));
+            // this._setValue(this.$input.val());
+        }
+        this.amount = this._compute_quantity(total);
+        // this.amount = this.format_weight(total)
+        // if (this.start_add) {
+        //     this.amount += this.props.val;
+        // }
+        // this.$input.val(this.amount.toLocaleString(this.locale_code));
+
+        // this._setValue(this.$input.val());
+        // this.props.update(this.amount);
+        debugger;
+        this.props.update(this.amount);
+    }
+
+
+    /* TARE METHODS */
+
+    _updateTare(tare) {
+        // this.$tare_amount.text(this.format_weight(this.tare));
+        // this.$real_amount.text(this.format_weight(this.amount));
+        debugger;
+        this.tare += tare
+        this.state.tare = this.format_weight(this.tare);
+        // this._setMeasure(this.amount);
+        this._setMeasure(this.props.value);
+    }
+
+    /**
+     * Add the tare to the total weight
+     * @param {Event} ev
+     */
+    _onClickTare(ev) {
+        ev.preventDefault();
+        const tare = parseFloat(ev.currentTarget.dataset.tare);
+        this._updateTare(tare);
     }
 }
 RemoteMeasureFormOwl.template = "stock_weighing_remote_measure.RemoteMeasureFormOwl";
