37789bc394fc604edad00ecb5d8dfb10571e813e
javierjcf <javier@comunitea.com>
2024-09-11 16:24:22 +0200

    [ADD]web_widget_auto_measure: Mejora conexión reconexión sockets

--- web_widget_auto_measure/models/stock_move.py
+++ web_widget_auto_measure/models/stock_move.py
@@ -1,9 +1,45 @@
 # Copyright 2023 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.tools.misc import clean_context
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    auto_weight = fields.Float('Auto weight', default='69')
+    mode_auto_weighing = fields.Boolean('Auto weighing mode', default=False)
+    auto_weight = fields.Float('Auto weight')
+
+    @api.model
+    def set_auto_weight(self, move_id, weight):
+        move = self.browse(move_id)
+        vals =move._prepare_move_line_vals(quantity=self.weight)
+        # Avoid filling the reserved quantities
+        vals.pop("reserved_uom_qty", None)
+        # if self.lot_id:
+        #     vals["lot_id"] = self.lot_id.id
+        # self._check_lot_creation()
+        selected_line = (
+            self.env["stock.move.line"]
+            .with_context(**clean_context(self.env.context))
+            .create(vals)
+        )
+        if weight:
+            selected_line.qty_done = self.weight
+            selected_line.recorded_weight = self.weight
+            selected_line.has_recorded_weight = True
+            selected_line.weighing_user_id = self.env.user
+            selected_line.weighing_date = fields.Datetime.now()
+        # Reset value
+        else:
+            selected_line.qty_done = 0
+            selected_line.recorded_weight = 0
+            selected_line.has_recorded_weight = False
+            selected_line.weighing_user_id = False
+            selected_line.weighing_date = False
+        # Unlock the operation
+        selected_line.move_id.action_unlock_weigh_operation()
+        # action = selected_line.action_print_weight_record_label()
+        # action["close_on_report_download"] = True
+        # return action
+
--- web_widget_auto_measure/static/src/auto_measure/auto_measure.esm.js
+++ web_widget_auto_measure/static/src/auto_measure/auto_measure.esm.js
@@ -1,18 +1,20 @@
 /** @odoo-module **/
-import {registry} from "@web/core/registry";
+import { registry } from "@web/core/registry";
 const { useState } = owl;
-import {useService} from "@web/core/utils/hooks";
-
-import {RemoteMeasureOwl} from "@web_widget_remote_measure/remote_measure/remote_measure.esm";
-console.log('DEBERIA SER EL 2-2')
-
+import { useService } from "@web/core/utils/hooks";
+import { RemoteMeasureOwl } from "@web_widget_remote_measure/remote_measure/remote_measure.esm";
 
 export class AutoMeasureWidget extends RemoteMeasureOwl {
     setup() {
+        debugger;
         super.setup();
         this.value = 0;
         this.rpc = this.env.services.rpc;
         this.orm = useService("orm");
+        this.reconnectionDelay = 1000;  // Inicialmente 5 segundos
+        this.maxReconnectionDelay = 60000;  // Límite de 1 minuto para el backoff exponencial
+        this.keepAliveInterval = 30000;  // Ping cada 30 segundos
+        this._connect_to_websockets2();  // Conectar al iniciar el componente
     }
 
     get circleColor() {
@@ -20,141 +22,126 @@ export class AutoMeasureWidget extends RemoteMeasureOwl {
         return this.value > 0 ? 'red' : 'green';
     }
 
-    async loadRemoteDeviceData() {
-        // Llamamos al método original para cargar los datos del dispositivo remoto
-        await super.loadRemoteDeviceData();
-        console.log("**** loadRemoteDeviceData() ****");
-        this._connect_to_websockets2();  // Conectamos automáticamente el socket al cargar los datos
-    }
+    // async loadRemoteDeviceData() {
+    //     // Llamamos al método original para cargar los datos del dispositivo remoto
+    //     await super.loadRemoteDeviceData();
+    //     console.log("**** loadRemoteDeviceData() ****");
+    //     this._connect_to_websockets2();  // Conectamos automáticamente el socket al cargar los datos
+    // }
 
     _connect_to_websockets2() {
         console.log("**** _connect_to_websockets2() ****");
         try {
-            this.socket = new WebSocket(this.host);  // Conectamos al host
+            // if (this.socket) {  //
+            //     this.socket.close();  // Cerrar la conexión existente
+            // }
+            this.socket = new WebSocket(this.host);
+            this._setupWebSocketEvents();  // Configuramos los eventos del WebSocket
         } catch (error) {
             if (error.code === 18) {
                 return;  // Error de permisos en algunos navegadores
             }
             throw error;
         }
+    }
 
-        var stream_success_counter = 10;  // Contador de estabilidad
+    _setupWebSocketEvents() {
+        var stream_success_counter = 10;
+
+        this.socket.onopen = () => {
+            console.log("WebSocket connection established");
+            this._startKeepAlive();  // Iniciar pings de keep-alive
+            this.reconnectionDelay = 1000;  // Resetear el tiempo de reconexión al abrir la conexión
+        };
 
         this.socket.onmessage = async (msg) => {
             console.log("**** onmessage() (desde _connect_to_websockets2) ****");
-            console.log("stream_success_counter: ", stream_success_counter);
             const data = await msg.data.text();
-
-            // Procesar el mensaje según el protocolo definido (por ejemplo: _proccess_msg_f501)
             const processedData = this[`_proccess_msg_${this.protocol}`](data);
-            console.log("processedData: ", processedData);
 
             if (!processedData.stable) {
                 stream_success_counter = 5;  // Reinicia el contador si no es estable
             }
 
-            // Si los datos son estables y el contador es cero, llamamos a _stableMeasure
             if (processedData.stable && !stream_success_counter) {
-                this._stableMeasure();  // Almacenamos el valor estable, pero no cerramos el socket
-                this._awaitingMeasure();  // Ponemos el widget en modo "espera"
-                this._recordMeasure(processedData.value);  // Registramos la medida
-                console.log("**** Datos estables procesados, pero el socket sigue abierto ****");
+                this._stableMeasure();
+                this._awaitingMeasure();
+                this._recordMeasure(processedData.value);
                 return;
             }
 
-            // Si los datos no son estables, llamamos a _unstableMeasure
             this._unstableMeasure();
-
-            // Decrementamos el contador
-            if (stream_success_counter) {
-                --stream_success_counter;
-            }
-
-            // Actualizamos el icono y la medida
-            this.state.icon = this._nextStateIcon(this.state.icon);
+            if (stream_success_counter) --stream_success_counter;
             this.amount = processedData.value;
-            this._setMeasure();  // Establecemos la nueva medida
-            this._recordMeasure(processedData.value);  // Registramos la medida actual
+            this._setMeasure();
+            this._recordMeasure(processedData.value);
         };
 
         this.socket.onerror = () => {
-            // Manejo del error del socket
-            this._awaitingMeasure();
             console.error('WebSocket error');
-            this.notification.add(
-                this.env._t("Could not connect to WebSocket"),
-                {
-                    type: "danger",
-                }
-            );
+            this._awaitingMeasure();
+        };
+
+        this.socket.onclose = () => {
+            console.log("WebSocket connection closed. Trying to reconnect...");
+            this._stopKeepAlive();
+            this._reconnectWebSocket();  // Intentar reconectar
         };
     }
 
-    async _stableMeasure() {
-        super._stableMeasure();  // Llamamos a la lógica base para manejar la estabilidad
-        
-        // Comprobamos si el valor estable actual es mayor que 0 y el valor anterior era 0
-        if (this.state.isStable) {
-            let oldValue = this.value;
-            this.value = this.amount;  // Actualizamos el valor
-
-            if (oldValue === 0 && this.amount > 0){
-
-                
-                // Llamamos a doAction para abrir la vista del wizard
-                const action = {
-                    type: 'ir.actions.act_window',
-                    res_model: 'weighing.wizard',  // Nombre del modelo del wizard
-                    views: [[false, 'form']],  // Vista de formulario
-                    target: 'new',  // Abrir en una nueva ventana modal
-                    context: {
-                        active_id: 10,  // Pasamos el ID 10 al wizard como active_id
-                    }
-                };
-                
-                // Llamada a doAction, capturando el resultado
-                await this.env.services.action.doAction(action);
-                debugger;
-                
+    _startKeepAlive() {
+        this.keepAliveTimer = setInterval(() => {
+            if (this.socket.readyState === WebSocket.OPEN) {
+                this.socket.send("ping");
+                console.log("Keep-alive ping sent");
             }
-
-        // Procesamos el resultado de la acción
-        // if (result && result.res_model === 'weighing.wizard') {
-        //     console.log("Wizard cerrado, procesamos el resultado:", result);
-        // }
-
-
-
-        // this.orm.call(
-        //     "weighing.wizard",
-        //     "record_weight",
-        //     [1],  // Le pasamos el ID o los argumentos necesarios
-        //     {context: this.context}
-        // );
-
-        }
-    
-        // Actualizamos el valor del campo independientemente del valor
-        this.props.update(this.amount);
+        }, this.keepAliveInterval);
     }
 
+    _stopKeepAlive() {
+        clearInterval(this.keepAliveTimer);
+    }
 
+    _reconnectWebSocket() {
+        setTimeout(() => {
+            console.log(`Reconnecting WebSocket after ${this.reconnectionDelay / 1000} seconds...`);
+            this._connect_to_websockets2();
+            this.reconnectionDelay = Math.min(this.reconnectionDelay * 2, this.maxReconnectionDelay);  // Backoff exponencial
+        }, this.reconnectionDelay);
+    }
 
+    async _stableMeasure() {
+        debugger;
+        super._stableMeasure();
+        if (this.state.isStable) {
+            let oldValue = this.value;
+            this.value = this.amount;
 
-   
+            if (oldValue === 0 && this.amount > 0) {
+                let move_id = this.props.record.data.id
+                const action = {
+                    type: 'ir.actions.act_window',
+                    res_model: 'weighing.wizard',
+                    views: [[false, 'form']],
+                    target: 'new',
+                    context: { active_id: move_id }
+                };
+                /const wizardResult = await this.env.services.action.doAction(action
+                console.log("Creo operación")
+                // await this.orm.call("stock.move", "set_auto_weight", [move_id, this.value]);
+                // debugger;
+                // const viewAction = this.env.model.actionService.currentController.action;
+                // this.env.model.actionService.restore()
+                // this.env.model.actionService.doAction(viewAction, {});
+                // this.trigger_up('reload');  // Esto recarga la vista en la que está montado el componente
+            }
+        }
+        this.props.update(this.amount);
+    }
 }
 AutoMeasureWidget.template = "web_widget_auto_measure.AutoMeasureWidget";
 AutoMeasureWidget.props = {
     ...RemoteMeasureOwl.props,
 };
-
-const superExtractProps = RemoteMeasureOwl.extractProps;
-AutoMeasureWidget.extractProps = ({ attrs, field }) => {
-    return {
-        ...superExtractProps({attrs, field}),  // Geting Digits Precission
-    };
-};
-// AutoMeasureWidget.additionalClasses = ["weight_wizard"];
-// AutoMeasureWidget.components = { Dialog };
 
 registry.category("fields").add("auto_measure", AutoMeasureWidget);
