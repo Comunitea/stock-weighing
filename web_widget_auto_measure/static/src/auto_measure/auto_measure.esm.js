a9733caace5078b0e2fa2c10cf269f1c7f6d77d2
javierjcf <javier@comunitea.com>
2024-09-12 23:54:10 +0200

    [FIX]web_widget_auto_measures: Gestion de sockets y reactividad

--- web_widget_auto_measure/static/src/auto_measure/auto_measure.esm.js
+++ web_widget_auto_measure/static/src/auto_measure/auto_measure.esm.js
@@ -1,48 +1,72 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
-const { useState } = owl;
+const {onWillStart, onMounted, onWillUnmount, onWillDestroy, useState } = owl;
 import { useService } from "@web/core/utils/hooks";
 import { RemoteMeasureOwl } from "@web_widget_remote_measure/remote_measure/remote_measure.esm";
 
 export class AutoMeasureWidget extends RemoteMeasureOwl {
     setup() {
-        console.log("**** 2 setup() ****");
+        console.log("### 2 setup() ###");
         super.setup();
-        // this.value = 0;
-        this.value = this.props.value;
+        this.value = 0;  // this.amount es reactivo (esta comentado en el useState, pero no debería, se mete luego en el onmessage y el _recordMeasure())
+        // this.value = this.props.value;
         this.showWidget = this.env.config.viewType === "kanban" ? false : true;
 
         
         this.reconnectionDelay = 1000;  // Inicialmente 5 segundos
         this.maxReconnectionDelay = 60000;  // Límite de 1 minuto para el backoff exponencial
         this.keepAliveInterval = 30000;  // Ping cada 30 segundos
-        // if (this.showWidget) {
-        //     this._connect_to_websockets2();  // Conectar al iniciar el componente
-        // }
+
+        // Hooks can not be overwrited, so we use the original methods in super class
+        // if we declare the hooks in the subclass, they will be executed after the superclass
+
+        // onWillStart(async () => {
+        //     debugger;
+        //     console.log("### 2 onWillStart() ###");
+        // });
+
+        // onMounted(() => {
+        //     if (this.remote_device_data && this.remote_device_data.instant_read) {
+        //         this.measure();
+        //     }
+        // });
+        // onWillUnmount(() => {
+        //     console.log("### 2 onWillUnmount() ###");
+        //     this._closeSocket();
+        // });
+        // onWillDestroy(() => {
+        //     console.log("### 2 onWillDestroy() ###");
+        //     this._closeSocket();
+        // });
     }
 
     get circleColor() {
         // Si el valor es mayor que 0, rojo; si es 0 o menor, verde.
-        return this.value > 0 ? 'red' : 'green';
+        return this.amount > 0 ? 'red' : 'green';
     }
 
     async loadRemoteDeviceData() {
         // Llamamos al método original para cargar los datos del dispositivo remoto
         await super.loadRemoteDeviceData();
-        console.log("**** loadRemoteDeviceData() ****");
+        console.log("### loadRemoteDeviceData() ###");
         if (this.showWidget) {
             this._connect_to_websockets2();  // Conectar al iniciar el componente
         }
     }
 
     _connect_to_websockets2() {
-        console.log("**** _connect_to_websockets2() ****");
+        console.log("### _connect_to_websockets2() ###");
         try {
             // if (this.socket) {  //
             //     this.socket.close();  // Cerrar la conexión existente
             // }
-            this.socket = new WebSocket(this.host);
-            this._setupWebSocketEvents();  // Configuramos los eventos del WebSocket
+            if ( !this.socket) {
+                this.socket = new WebSocket(this.host);
+                this._setupWebSocketEvents();  // Configuramos los eventos del WebSocket
+            }
+            else {
+                console.log("WebSocket ya existe, ignorando...");
+            }
         } catch (error) {
             if (error.code === 18) {
                 return;  // Error de permisos en algunos navegadores
@@ -56,30 +80,38 @@ export class AutoMeasureWidget extends RemoteMeasureOwl {
 
         this.socket.onopen = () => {
             console.log("WebSocket connection established");
-            this._startKeepAlive();  // Iniciar pings de keep-alive
+            // this._startKeepAlive();  // Iniciar pings de keep-alive
             // this.reconnectionDelay = 1000;  // Resetear el tiempo de reconexión al abrir la conexión
         };
 
         this.socket.onmessage = async (msg) => {
-            console.log("**** onmessage() (desde _connect_to_websockets2) ****");
+            console.log("### onmessage() (desde _connect_to_websockets2) ###");
             const data = await msg.data.text();
             const processedData = this[`_proccess_msg_${this.protocol}`](data);
 
             if (!processedData.stable) {
                 stream_success_counter = 5;  // Reinicia el contador si no es estable
             }
-
+            
+            // STABLE MEASURE
             if (processedData.stable && !stream_success_counter) {
+                // Set the readed value to reactive amount field and use update promise to save in the record
                 this._stableMeasure();
-                this._awaitingMeasure();
-                this._recordMeasure(processedData.value);
+                // this._awaitingMeasure();
+                // this._recordMeasure(processedData.value);
                 return;
             }
-
+            // UNSTABLE MEASURE
+            // Update the reactive field amount without saving to record
+            // Setting reactive fiels isStable to false
             this._unstableMeasure();
             if (stream_success_counter) --stream_success_counter;
+
             this.amount = processedData.value;
+            // Set the readed value to reactive amount field and use update promise to save in the record
             this._setMeasure();
+
+            // Todo quitar?
             this._recordMeasure(processedData.value);
         };
 
@@ -89,31 +121,31 @@ export class AutoMeasureWidget extends RemoteMeasureOwl {
 
         this.socket.onclose = () => {
             console.log("WebSocket connection closed. Trying to reconnect...");
-            this._stopKeepAlive();
-            this._reconnectWebSocket();  // Intentar reconectar
+            // this._stopKeepAlive();
+            // this._reconnectWebSocket();  // Intentar reconectar
         };
     }
 
-    _startKeepAlive() {
-        this.keepAliveTimer = setInterval(() => {
-            if (this.socket.readyState === WebSocket.OPEN) {
-                this.socket.send("ping");
-                console.log("Keep-alive ping sent");
-            }
-        }, this.keepAliveInterval);
-    }
+    // _startKeepAlive() {
+    //     this.keepAliveTimer = setInterval(() => {
+    //         if (this.socket.readyState === WebSocket.OPEN) {
+    //             this.socket.send("ping");
+    //             console.log("Keep-alive ping sent");
+    //         }
+    //     }, this.keepAliveInterval);
+    // }
 
-    _stopKeepAlive() {
-        clearInterval(this.keepAliveTimer);
-    }
+    // _stopKeepAlive() {
+    //     clearInterval(this.keepAliveTimer);
+    // }
 
-    _reconnectWebSocket() {
-        setTimeout(() => {
-            console.log(`Reconnecting WebSocket after ${this.reconnectionDelay / 1000} seconds...`);
-            this._connect_to_websockets2();
-            this.reconnectionDelay = Math.min(this.reconnectionDelay * 2, this.maxReconnectionDelay);  // Backoff exponencial
-        }, this.reconnectionDelay);
-    }
+    // _reconnectWebSocket() {
+    //     setTimeout(() => {
+    //         console.log(`Reconnecting WebSocket after ${this.reconnectionDelay / 1000} seconds...`);
+    //         this._connect_to_websockets2();
+    //         this.reconnectionDelay = Math.min(this.reconnectionDelay * 2, this.maxReconnectionDelay);  // Backoff exponencial
+    //     }, this.reconnectionDelay);
+    // }
 
     async _stableMeasure() {
         super._stableMeasure();
@@ -130,21 +162,23 @@ export class AutoMeasureWidget extends RemoteMeasureOwl {
                     target: 'new',
                     context: { active_id: move_id }
                 };
-                const wizardResult = await this.env.services.action.doAction(action)
                 console.log("Creo operación")
-                // await this.orm.call("stock.move", "set_auto_weight", [move_id, this.value]);
+                await this.orm.call("stock.move", "set_auto_weight", [move_id, this.value]);
+                this.env.model.actionService.switchView('form')
+                
+                //  await this.env.services.action.doAction(action)
                 // const viewAction = this.env.model.actionService.currentController.action;
+                // this.env.model.actionService.loadAction(viewAction.id, {});
                 // await this.env.model.actionService.doAction({
                 //     type: 'ir.actions.client',
                 //     tag: 'reload',
                 // });
                 // this.env.model.actionService.restore()
                 // this.env.model.actionService.doAction(viewAction, {});
-                // this.env.model.actionService.switchView('form')
-                // this.env.model.actionService.loadAction(viewAction.id, {});
                 // this.trigger_up('reload');  // Esto recarga la vista en la que está montado el componente
             }
         }
+        debugger;
         this.props.update(this.amount);
         // this.props.update(this.value);
     }
